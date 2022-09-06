3210e624c4f514918bd0cba18bdac2c9f39635be
Gerard Keiser <gerardkeiser@gmail.com>
2022-09-06 10:06:01 -0500

    Make register function

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,13 +1,41 @@
 /** User class for message.ly */
 
+import db from "../db";
+import bcrypt from "bcrypt";
+
+const { SECRET_KEY, BCRYPT_WORK_FACTOR } = require("../config");
+
 /** User of the site. */
+
+interface UserData {
+  username: string;
+  password: string;
+  first_name: string;
+  last_name: string;
+  phone: string;
+}
 
 class User {
   /** register new user -- returns
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({ username, password, first_name, last_name, phone }) {}
+  static async register({
+    username,
+    password,
+    first_name,
+    last_name,
+    phone,
+  }: UserData): Promise<UserData> {
+    const hashed = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    const result = await db.query(
+      `INSERT INTO users (username, password, first_name, last_name, phone)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING username, password, first_name, last_name, phone`,
+      [username, hashed, first_name, last_name, phone]
+    );
+    return result.rows[0];
+  }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
