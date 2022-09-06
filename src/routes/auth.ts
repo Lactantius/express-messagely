e8f93bed5db6027ec04b7bde6b1fbbac24af3410
Gerard Keiser <gerardkeiser@gmail.com>
2022-09-06 17:27:12 -0500

    Make login route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,35 @@
+import { Router } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import db from "../db";
+
+const router = Router();
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const result = await db.query(
+      `SELECT password FROM users WHERE username=$1`,
+      [username]
+    )
+    const user = result.rows[0]
+    if (user) {
+      if (await bcrypt.compare(password, user.password)) {
+        const token = jwt.sign({ username }, SECRET_KEY);
+        return res.json({ token });
+      }
+    }
+    throw new ExpressError("Invalid username or password", 400);
+  } catch (err) {
+    return next(err);
+  }
+})
 
 
 /** POST /register - register user: registers, logs in, and returns token.
